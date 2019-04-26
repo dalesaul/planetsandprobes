86d365565cf967bf2cf5be50cd0de0902e1f0cc8
Dale <dalesauljr@gmail.com>
2019-04-26 15:02:44 -0400

    technically incorrect, but saving progress for now

---
+++ Probes.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace planets
+{
+    class Probes
+    {
+
+public Probes(string probeParam, string planetParam){
+    probeName = probeParam;
+    planetName = planetParam;
+}
+
+public string probeName {get; set;}
+public string planetName {get; set;}
+
+}
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        List<string> planetList = new List<string>(){
+            "Mercury",
+            "Mars"
+        };
+
+        planetList.Add("Jupiter");
+        planetList.Add("Saturn");
+
+        List<string> outterPlanets = new List<string>(){
+            "Uranus",
+            "Neptune"
+        };
+
+        planetList.AddRange(outterPlanets);
+
+        planetList.Insert(1, "Venus");
+        planetList.Insert(2, "Earth");
+
+        planetList.Add("Pluto");
+
+
+        // string[] output = planetList.GetRange(0, 4).ToArray();
+        // foreach (string rockyPlanet in output){
+        // Console.WriteLine(output);
+        // }
+        // planetList.Remove("Pluto");
+
+        foreach (string planet in planetList)
+        Console.WriteLine(planet);
+
+        Probes viking = new Probes("Viking", "Mars");
+        Probes opportunity = new Probes("Opportunity", "Mars");
+        Probes curiosity = new Probes("Curiosity", "Mars");
+        Probes mariner = new Probes("Mariner", "Venus");
+        Probes venera = new Probes("Venera", "Venus");
+
+List<Probes> probeDestinations = new List<Probes>(){
+viking,
+curiosity,
+mariner,
+venera,
+opportunity
+
+};
+
+
+
+foreach(Probes probe in probeDestinations)
+Console.WriteLine($"{probe.probeName} : {probe.planetName}" );
+
+                }
+            }}
+
+
+
+
